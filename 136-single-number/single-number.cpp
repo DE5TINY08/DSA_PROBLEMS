55aac2fbc989636961266cd1db6a044a39a37463
DE5TINY08 <paratekrish@gmail.com>
2026-08-22 22:55:44 +0530

    Time: 0 ms (100.00%) | Memory: 20.7 MB (59.09%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,15 +1,10 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        map<int,int> check;
-        for(int i =0;i<nums.size();i++){
-            check[nums[i]] += 1;
+        int x = 0;
+        for(int i : nums){
+            x = x ^ i;
         }
-        for(auto const& [key, val] : check) {
-            if(val == 1) {
-                return key; 
-            }
-        }
-        return -1;
+        return x;
     }
 };
