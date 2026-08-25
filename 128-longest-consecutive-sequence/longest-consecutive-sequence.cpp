3ea9a8c85e35a9949291cb083498e8ab704788a1
DE5TINY08 <paratekrish@gmail.com>
2026-08-26 00:39:39 +0530

    Time: 81 ms (54.52%) | Memory: 89 MB (25.28%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -4,19 +4,19 @@ public:
         if(nums.empty()) return 0;
         if(nums.size() == 1) return 1;
         unordered_set<int> check(nums.begin(), nums.end());
-        int max_len = 1;
+        int maxi = 1;
         for(int num : check) {
             if(check.count(num - 1) == 0) {
-                int current_num = num;
-                int current_len = 1;
-                while(check.count(current_num + 1)) {
-                    current_num++;
-                    current_len++;
+                int current = num;
+                int len = 1;
+                while(check.count(current + 1)) {
+                    current++;
+                    len++;
                 }
-                max_len = max(max_len, current_len);
+                maxi = max(maxi, len);
             }
         }
         
-        return max_len;
+        return maxi;
     }
 };
